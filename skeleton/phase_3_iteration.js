f8a4a5165d085dfd17bb86d22c806e59cb3ac837
wc2184 <68214261+wc2184@users.noreply.github.com>
2022-09-07 16:58:34 -0400

    iteration fix

--- skeleton/phase_3_iteration.js
+++ skeleton/phase_3_iteration.js
@@ -1,15 +1,19 @@
-Array.prototype.bubbleSort = function(){
-    let sorted = false
-    while (!sorted) {
-        sorted = true
-        let i = 0
-        while ( i < this.length -1) {
-            if (this[i] > this[i + 1]){
-                this[i],this[i + 1] = this[i + 1], this[i]
-                sorted = false
-            }
-            i += 1
-        }
+Array.prototype.bubbleSort = function () {
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    let i = 0;
+    while (i < this.length - 1) {
+      if (this[i] > this[i + 1]) {
+        // this[i], (this[i + 1] = this[i + 1]), this[i];
+        // let temp = this[i];
+        // this[i] = this[i + 1];
+        // this[i + 1] = temp;
+        [this[i], this[i + 1]] = [this[i + 1], this[i]];
+        sorted = false;
+      }
+      i += 1;
     }
-    return this
-}
+  }
+  return this;
+};
