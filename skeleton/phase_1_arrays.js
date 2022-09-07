c41a9524da60f1baa77ac24c66f493f7302430ef
Xiangwei Liu <xiangweimg@gmail.com>
2022-09-07 15:24:58 -0400

    phase 1

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -7,3 +7,32 @@ Array.prototype.uniq = function () {
   });
   return newArray;
 };
+
+Array.prototype.twoSum = function () {
+  let result = [];
+  // this.forEach( function (el, idx) {
+    for(let a = 0; a < this.length; a++) {
+      for(let b = 0; b < this.length; b++) {
+        if (b !== a && b < a && this[b] + this[a] === 0) {
+          result.push([b, a])
+        }
+      }
+  }
+  // } 
+  // )
+  return result
+}
+
+
+Array.prototype.transpose = function () {
+  let result = [];
+  for (let i = 0; i < this[0].length; i++){
+    let line = [];
+    for (let j = 0; j < this.length; j++){
+      line.push(this[j][i])
+    }
+    result.push(line)
+  }
+  return result 
+}
+
